d012f471ae4c2d1fec1b60851ded10406d0f5fd9
wchamberlain8 <w.chamberlain1@lancaster.ac.uk>
2024-11-21 13:33:04 +0000

    Initial commit of resources

---
+++ custom-api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+#initialise the FastAPI
+app = FastAPI()
+
+#data model if we want to pass some data to the API
+class InputModel(BaseModel):
+    input_value: str
+
+#an api endpoint to test it works
+@app.post("/test")
+async def test(input: InputModel):
+    if input.input_value == "test":
+        return {"hello world"}
