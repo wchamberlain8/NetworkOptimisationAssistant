b3bba3b502203476efc72b9417a62d9ab222f9ca
wchamberlain8 <w.chamberlain1@lancaster.ac.uk>
2024-11-22 20:53:18 +0000

    Added in comparing definitions intent

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -8,7 +8,21 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from dictionary import TERM_DICTIONARY, COMPARISON_DICTIONARY
+#from resource_bank import TERM_DICTIONARY, COMPARISON_DICTIONARY
+
+TERM_DICTIONARY = {
+    "broadband": "Broadband is blah blah blah",
+    "bandwidth": "Bandwidth is the maximum rate of data transfer across a network",
+    "ethernet": "Ethernet is a wired connection that is faster and less susceptible to interference",
+    "wifi" : "Wi-Fi is a wireless connection that is very convenient but can be slower and less reliable due to interference or signal loss.",
+    #ADD MORE, LOTS MORE HERE
+}
+
+COMPARISON_DICTIONARY = {
+    "bandwidthbroadband": "Bandwidth is the maximum rate of data transfer across a network, whereas broadband is a high-speed internet connection that is always on.",
+    "ethernetwifi": "Ethernet is a wired connection that is faster and more reliable than Wi-Fi, which is a wireless connection that is more convenient but slower.",
+    #ADD MORE, LOTS MORE HERE
+}
 
 class ActionHelloWorld(Action):
 
@@ -51,8 +65,8 @@ class ActionCompareTerms(Action):
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        term1 = tracker.get_slot("term1")
-        term2 = tracker.get_slot("term2")
+        term1 = tracker.get_slot("comparison_term1")
+        term2 = tracker.get_slot("comparison_term2")
 
         if term1 and term2:
             key = "".join(sorted([term1.lower(), term2.lower()]))
