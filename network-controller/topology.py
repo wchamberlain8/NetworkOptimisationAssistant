fd4ea51ce21f2b03827db59c3495e30e125f1d93
wchamberlain8 <william.chamberlain8@gmail.com>
2025-03-20 20:37:50 +0000

    Updating topo

--- network-controller/topology.py
+++ network-controller/topology.py
@@ -13,12 +13,12 @@ class TestTopology(Topo):
         # Add the central switch
         s1 = self.addSwitch('s1')
 
-        # Connect h2, h3, h4 to the switch
-        for h in range(4, 10):
+        # Connect hosts
+        for h in range(3, 10):
             host = self.addHost(f'h{h}', mac=f'00:00:00:00:00:0{h}')
             self.addLink(s1, host, cls=TCLink, bw=40, delay='15ms')
 
-        # Add additional hosts and links (with greater bandwidth)
+        # Add extra hosts with varying link conditions
         h1 = self.addHost('h1', mac=f'00:00:00:00:00:01')
         h2 = self.addHost('h2', mac=f'00:00:00:00:00:02')
         h10 = self.addHost('h10', mac=f'00:00:00:00:00:10')
@@ -34,44 +34,7 @@ class TestTopology(Topo):
         self.addLink(s1, sus1, cls=TCLink, bw=50, delay='20ms')
         #self.addLink(s1, sus2, cls=TCLink, bw=50, delay='20ms')
 
-class RealisticTopology(Topo):
     
-    def build(self):
-        # Add the central switch
-        s1 = self.addSwitch('s1')
-
-        # Connect hosts with varying bandwidth and delay
-        hosts = [
-            ('h1', '00:00:00:00:00:01', 80, '10ms'),
-            ('h2', '00:00:00:00:00:02', 60, '15ms'),
-            ('i1', '00:00:00:00:00:03', 20, '20ms'),
-            ('h3', '00:00:00:00:00:04', 90, '12ms'),
-            ('h4', '00:00:00:00:00:05', 100, '5ms'),
-            ('i2', '00:00:00:00:00:06', 10, '25ms'),
-            ('i3', '00:00:00:00:00:07', 30, '18ms'),
-            ('h5', '00:00:00:00:00:08', 70, '13ms'),
-            ('i4', '00:00:00:00:00:09', 25, '22ms'),
-            ('i5', '00:00:00:00:00:0A', 15, '28ms'),
-            ('h6', '00:00:00:00:00:0B', 65, '16ms'),
-            ('h7', '00:00:00:00:00:0C', 35, '19ms'),
-            ('h8', '00:00:00:00:00:0D', 95, '8ms'),
-            ('h9', '00:00:00:00:00:0E', 12, '26ms'),
-            ('h10', '00:00:00:00:00:0F', 75, '14ms'),
-            ('h11', '00:00:00:00:00:10', 40, '17ms'),
-            ('i6', '00:00:00:00:00:11', 28, '21ms'),
-            ('i7', '00:00:00:00:00:12', 8, '29ms'),
-            ('h12', '00:00:00:00:00:13', 85, '11ms'),
-            ('i8', '00:00:00:00:00:14', 50, '16ms'),
-            ('i9', '00:00:00:00:00:15', 18, '23ms'),
-            ('web1', '00:00:00:00:00:16', 100, '5ms'),
-            ('sus1', '00:00:00:00:00:17', 80, '5ms'),
-            ('sus2', '00:00:00:00:00:18', 80, '5ms')
-        ]
-
-        for name, mac, bw, delay in hosts:
-            host = self.addHost(name, mac=mac)
-            self.addLink(s1, host, cls=TCLink, bw=bw, delay=delay)
-
 def simulateTraffic(net):
     "simulateTraffic is a command that will simulate background traffic on the network"
     
